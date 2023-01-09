d253c8245dfe595ebc9c925faeb7b4cf97658cbb
ouluqiang <ouluqaing@qq.com>
2023-01-09 18:18:20 +0800

    数据处理

--- app/src/main/java/com/leontsai/timerulerview/JsonString.kt
+++ app/src/main/java/com/leontsai/timerulerview/JsonString.kt
@@ -17,6 +17,7 @@ object JsonString {
        var json= StringUtils.getFromAssets(context,"20230106.m3u8")
         if (!json.isNullOrEmpty()){
            var discontinuity= "#EXT-X-DISCONTINUITY"
+           var extinf= "#EXTINF:"
             var start=json.indexOf(discontinuity)
             var end=json.indexOf("#EXT-X-ENDLIST")
             var s=json.subSequence(start+discontinuity.length,end)
@@ -24,7 +25,7 @@ object JsonString {
             if (s.contains(discontinuity)){
                 var extinfs= s.split(discontinuity)
                 for (i in extinfs.indices){
-                    var times=extinfs[i].split("#EXTINF:")
+                    var times=extinfs[i].split(extinf)
                     var list= mutableListOf<String>()
                     for (j in times.indices) {
 //                        Log.d("ss", "$i    ${times[j]}")
@@ -35,7 +36,7 @@ object JsonString {
                     map.put(i,list)
                 }
             }else{
-                var times=s.split("#EXTINF:")
+                var times=s.split(extinf)
                 for (j in times.indices) {
 //                    Log.d("ss", "${times[j]}")
                     var list= arrayListOf<String>()
@@ -47,25 +48,23 @@ object JsonString {
             }
             var mapTime= mutableMapOf<Int,MutableList<String>>()
             for (i in 0 until map.size) {
-                Log.d("ss", "${map[i]}")
+//                Log.d("ss", "${map[i]}")
                 var list= arrayListOf<String>()
-//                if ((map[i]?.size ?: 0) > 1){
-                    var times=map[i]
-                    for (j in 0 until times!!.size) {
+                var times=map[i]
+                for (j in 0 until times!!.size) {
 //                        var other=".ts"
-                        if (times!!.size>1){
-                            var dates=times[j].split(",")
-                            var date=dates[1].substring(dates[1].lastIndexOf("/")+1,dates[1].lastIndexOf(".ts"))
-                            list.add(date)
-                        }else{
-                            var dates=times[j].split(",")
-                            var date=dates[1].substring(dates[1].lastIndexOf("/")+1,dates[1].lastIndexOf(".ts"))
-                            var endDate=StringUtils.countingString((date.toLong()+dates[0].toDouble()).toString())
-                            list.add(date)
-                            list.add(endDate)
-                        }
-
+                    if (times!!.size>1){
+                        var dates=times[j].split(",")
+                        var date=dates[1].substring(dates[1].lastIndexOf("/")+1,dates[1].lastIndexOf(".ts"))
+                        list.add(date)
+                    }else{
+                        var dates=times[j].split(",")
+                        var date=dates[1].substring(dates[1].lastIndexOf("/")+1,dates[1].lastIndexOf(".ts"))
+                        var endDate=StringUtils.countingString((date.toLong()+dates[0].toDouble()).toString())
+                        list.add(date)
+                        list.add(endDate)
                     }
+                }
                 mapTime.put(i,list)
             }
             return mapTime
